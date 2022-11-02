cb9d03a71bad57a8f54dc7d9c8daf8959d99cfdf
Senpa17 <117304298+Senpa17@users.noreply.github.com>
2022-11-02 22:14:01 +0500

    Add files via upload

---
+++ architecture/app/js.js
@@ -0,0 +1,8 @@
+function superClick(el) {
+			el.style.background = 'red';
+			el.style.color = 'white';
+		}
+function changeDisplay(){
+			const header3 = document.getElementById("1"):
+			header3.style.background = "yellow";
+		}
---
+++ architecture/app/scripts/script.js
@@ -0,0 +1,5 @@
+
+function changeDisplay(){
+			const header3 = document.getElementById("1"):
+			header3.style.background = "yellow";
+		}
