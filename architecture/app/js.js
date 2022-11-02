371e4f86dae54eafd0023b613bf3055e885f4636
Senpa17 <117304298+Senpa17@users.noreply.github.com>
2022-11-02 22:17:05 +0500

    Delete js.js

--- architecture/app/js.js
+++
@@ -1,8 +0,0 @@
-function superClick(el) {
-			el.style.background = 'red';
-			el.style.color = 'white';
-		}
-function changeDisplay(){
-			const header3 = document.getElementById("1"):
-			header3.style.background = "yellow";
-		}
